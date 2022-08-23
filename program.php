adaba2675311a2a18af13ad45923e73b0f35c46c
inna <inna.obraztsovas@gmail.com>
2022-08-23 17:18:17 +0300

    lala

--- program.php
+++ program.php
@@ -1,33 +1,38 @@
 <?php
 
-while(true) {
 
-    $firstCmd = readLine("Количетво рабочих дней в месяц:");
-    if (!empty($firstCmd && is_int($firstCmd))) {
-        readline_add_history($firstCmd);
-    }
+$firstCmd = readLine("Количетво рабочих дней в месяц:");
+$secondCmd = readLine("Цена за урок с человека:");
+$thirdCmd = readLine("Количество учеников в группе:");
+$fourthCmd = readLine("Количество уроков в день:");
 
-    $secondCmd = readLine("Цена за урок с человека:");
-    if (!empty($secondCmd && is_int($secondCmd))) {
-        readline_add_history($secondCmd);
-    }
+$fullAmount = $firstCmd * $secondCmd * $thirdCmd * $fourthCmd;
 
-    $thirdCmd = readLine("Количество учеников в группе:");
-    if (!empty($thirdCmd && is_int($thirdCmd))) {
-        readline_add_history($thirdCmd);
-    }
-
-    $fourthCmd = readLine("Количество уроков в день:");
-    if (!empty($fourthCmd && is_int($fourthCmd))) {
-        readline_add_history($fourthCmd);
-    }
-
-
-    $fullAmount = $firstCmd * $secondCmd * $thirdCmd * $fourthCmd;
-    $halfAmount = ($firstCmd * $secondCmd * $thirdCmd * $fourthCmd) / 2;
-    $trtPrsntAmount = ($firstCmd * $secondCmd * $thirdCmd * $fourthCmd) * 30 / 100;
-
-    echo "Выручка 100%:" . $fullAmount . PHP_EOL;
-    echo "Выручка 50%:" . $halfAmount . PHP_EOL;
-    echo "Выручка 30%:" . $trtPrsntAmount . PHP_EOL;
+function calculatePersent($per, $amount)
+{
+    return $amount*$per / 100;
 }
+
+function tax($tax,$amount)
+{
+    return $amount - $amount*($tax/100);
+}
+$hundred = calculatePersent(100,$fullAmount);
+$thirty = calculatePersent(30,$fullAmount);
+$forty = calculatePersent(40,$fullAmount);
+$fifty = calculatePersent(50,$fullAmount);
+$sixty = calculatePersent(60,$fullAmount);
+$seventy = calculatePersent(70,$fullAmount);
+
+$m = tax(5,$hundred);
+$f = tax(5,$fifty);
+$z = tax(5,$thirty);
+
+
+echo "Доход 100%:" . $hundred . PHP_EOL;
+echo "Прибыль 100%" .$m . PHP_EOL;
+echo "Доход 50%:" . $fifty . PHP_EOL;
+echo "Прибыль 50%" .$f . PHP_EOL;
+echo "Доход 30%:" . $thirty . PHP_EOL;
+echo "Прибыль 30%:" . $z . PHP_EOL;
+
