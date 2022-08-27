149ec0647250610052ac511e61ebb095dc2c9d7c
inna <inna.obraztsovas@gmail.com>
2022-08-27 21:09:33 +0300

    remade to oop improved exceptions

--- index.php
+++ index.php
@@ -1,55 +1,112 @@
 <?php
 
-$input = $_REQUEST;
-$questions = [
-    'days' => "Количетво рабочих дней в месяц:",
-    'price' => "Цена за урок с человека:",
-    'students' => "Количество учеников в группе:",
-    'lessons' => "Количество уроков в день:"
-];
+class CLI
+{
+    private const QUESTIONS = [
+        'days' => "Количетво рабочих дней в месяц:",
+        'price' => "Цена за урок с человека:",
+        'students' => "Количество учеников в группе:",
+        'lessons' => "Количество уроков в день:"
+    ];
 
-$validationCountDays = [
-    'days'
-];
-
-foreach ($questions as $key => $question) {
-    $input[$key] = readLine($question);
-    validateInput($input[$key]);
-    if (in_array($key, $validationCountDays)) {
-        validateCountDays($input[$key]);
+    public function logic(): void
+    {
+        $service = new Service();
+        $calculate = new Calculate();
+        $result = $service->writeCommand(self::QUESTIONS);
+        $fullAmount = $calculate->calculateFullAmount($result);
+        $calculate->dataOutput($fullAmount);
     }
 }
 
-function validateInput(mixed $data): void
+class Service
 {
-    if (!empty($data) && !is_numeric($data) || $data <= 0) {
-        throw new \Exception('Needle numbers');
+    private const VALIDATE_COUNT_DAYS = [
+        'days'
+    ];
+
+    private string $errorKey;
+
+    /**
+     * @return string
+     */
+    public function getErrorKey(): string
+    {
+        return $this->errorKey;
     }
-}
 
-function validateCountDays(int $days): void
-{
-    if ($days > 31) {
-        throw new \Exception('Max value is 31');
+    /**
+     * @param string $errorKey
+     */
+    public function setErrorKey(string $errorKey): void
+    {
+        $this->errorKey = $errorKey;
     }
+
+    public function writeCommand(array $questions): array
+    {
+        $input = [];
+        foreach ($questions as $key => $question) {
+            try {
+                $input[$key] = readLine($question);
+                $this->validateInput($input[$key]);
+                if (in_array($key, self::VALIDATE_COUNT_DAYS)) {
+                    $this->validateCountDays($input[$key]);
+                }
+
+            } catch (Throwable $e) {
+                ;
+                $this->setErrorKey($key);
+                readline($e->getMessage());
+                $input[$key] = readLine($question);
+            }
+        }
+        return $input;
+    }
+
+    private function validateInput(mixed $data): void
+    {
+        if (!empty($data) && !is_numeric($data) || $data <= 0) {
+            throw new \Exception('Needle numbers');
+        }
+    }
+
+
+    private function validateCountDays(int $days): void
+    {
+        if ($days > 31) {
+            throw new \Exception('Max value is 31');
+        }
+    }
+
 }
 
-$fullAmount = $input['days'] * $input['price'] * $input['price'] * $input['lessons'];
-
-
-function calculatePersent(int $per, int|float $amount): int|float
+class Calculate
 {
-    return $amount * $per / 100;
+    public function calculateFullAmount(array $data): int|float
+    {
+        return $data['days'] * $data['price'] * $data['students'] * $data['lessons'];
+    }
+
+    private function calculatePersent(int $per, int|float $amount): int|float
+    {
+        return $amount * $per / 100;
+    }
+
+    private function tax(int $tax, int|float $amount): int|float
+    {
+        return $amount - $amount * ($tax / 100);
+    }
+
+    public function dataOutput(int|float $fullAmount): void
+    {
+        for ($i = 30; $i < 80; $i += 10) {
+            echo "Доход " . $i . "%: " . $grossProfit = $this->calculatePersent($i, $fullAmount) . PHP_EOL;;
+            echo "Прибыль " . $i . "%: " . $this->tax(5, $grossProfit) . PHP_EOL;;
+        }
+    }
+
 }
 
-function tax(int $tax, int|float $amount): int|float
-{
-    return $amount - $amount * ($tax / 100);
-}
-
-for ($i = 30; $i < 80; $i += 10) {
-    echo "Доход " . $i . "%: " . $grossProfit = calculatePersent($i, $fullAmount) . PHP_EOL;
-    echo "Прибыль " . $i . "%: " . $netProfit = tax(5, $grossProfit) . PHP_EOL;
-}
-
-
+$a = new CLI();
+$a->logic();
