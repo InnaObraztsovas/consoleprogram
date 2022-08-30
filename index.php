1bf10ef0d42b00cc87783a924af2666099ca2b60
inna <inna.obraztsovas@gmail.com>
2022-08-30 11:30:53 +0300

    added json post data

--- index.php
+++ index.php
@@ -1,19 +1,58 @@
 <?php
 
-interface Command{
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $the_file = fopen("input.json", 'wb') or die("Unable to open file!");
+    fwrite($the_file, json_encode($data, JSON_THROW_ON_ERROR));
+    fclose($the_file);
+    echo "success";
+}
 
+class OutputData
+{
+
+    public function logic()
+    {
+        $inputData = new InputData();
+        $calculate = new Calculate();
+        $result = $inputData->getData(CLI::QUESTIONS);
+        $fullAmount = $calculate->calculateFullAmount($result);
+        $output = $calculate->dataOutputJson($fullAmount);
+        print_r($output);
+    }
+}
+
+class InputData
+{
+
+    public function getData(array $data)
+    {
+        $service = new Service();
+        $str = file_get_contents('http://localhost/input.json');
+        $data = json_decode($str, true);
+        $input = [];
+        foreach ($data as $json) {
+            foreach ($json as $key => $value) {
+                $input[$key] = $value;
+                $service->validateInput($input[$key]);
+                if (in_array($key, Service::VALIDATE_COUNT_DAYS)) {
+                    $service->validateCountDays($input[$key]);
+                }
+            }
+        }
+        return $input;
+    }
+}
+
+
+class CLI
+{
     public const QUESTIONS = [
         'days' => "Количетво рабочих дней в месяц:",
         'price' => "Цена за урок с человека:",
         'students' => "Количество учеников в группе:",
         'lessons' => "Количество уроков в день:"
     ];
-
-    public function logic();
-
-}
-class CLI implements Command
-{
 
     public function logic(): void
     {
@@ -27,7 +66,7 @@ class CLI implements Command
 
 class Service
 {
-    private const VALIDATE_COUNT_DAYS = [
+    public const VALIDATE_COUNT_DAYS = [
         'days'
     ];
 
@@ -67,7 +106,7 @@ class Service
         return $input;
     }
 
-    private function validateInput(mixed $data): void
+    public function validateInput(mixed $data): void
     {
         if (!empty($data) && !is_numeric($data) || $data <= 0) {
             throw new \Exception('Needle numbers');
@@ -75,7 +114,7 @@ class Service
     }
 
 
-    private function validateCountDays(int $days): void
+    public function validateCountDays(int $days): void
     {
         if ($days > 31) {
             throw new \Exception('Max value is 31');
@@ -104,12 +143,26 @@ class Calculate
     public function dataOutput(int|float $fullAmount): void
     {
         for ($i = 30; $i < 80; $i += 10) {
-            echo "Доход " . $i . "%: " . $grossProfit = $this->calculatePersent($i, $fullAmount) . PHP_EOL;;
-            echo "Прибыль " . $i . "%: " . $this->tax(5, $grossProfit) . PHP_EOL;;
+            echo "Доход " . $i . "%: " . $grossProfit = $this->calculatePersent($i, $fullAmount) . PHP_EOL;
+            echo "Прибыль " . $i . "%: " . $this->tax(5, $grossProfit) . PHP_EOL;
         }
     }
 
+    public function dataOutputJson($fullAmount)
+    {
+        $result = [];
+        for ($i = 30; $i < 80; $i += 10) {
+            $result[$i] = [
+                "Доход " . $i . "%: " => $grossProfit = $this->calculatePersent($i, $fullAmount) . PHP_EOL,
+                "Прибыль " . $i . "%: " => $this->tax(5, $grossProfit) . PHP_EOL
+            ];
+        }
+        return json_encode($result);
+    }
 }
 
-$a = new CLI();
-$a->logic();
+//$a = new CLI();
+//$a->logic();
+
+$b = new OutputData();
+$b->logic();
